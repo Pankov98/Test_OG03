24e365b40861d1d768f3e863082b6f20df49d6be
Pankov98 <n.pankov@list.ru>
2024-12-19 19:22:20 +0300

    the sum of the angles

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ def calculate_radius_from_area(area):
 
     radius = math.sqrt(area / math.pi)
     return radius
+
+
+def sum_of_interior_angles(n):
+    if n < 3:
+        raise ValueError("Многоугольник должен иметь как минимум 3 стороны")
+
+    # Используем формулу для нахождения суммы внутренних углов
+    sum_angles = (n - 2) * 180
+    return sum_angles
